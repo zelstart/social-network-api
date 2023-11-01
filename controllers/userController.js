c1b322e196f5d1fe0f24a6b9b125c4c7e8b6f5e6
zelstart <e.pomeroy95@gmail.com>
2023-10-31 23:13:56 -0400

    get all, get by id and post new user all work

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
 const User  = require('../models/User');
 
 module.exports = {
-    // api/users/ -- gets all users
+    // /api/users/ -- gets all users
     async getUsers(req, res) {
         try {
             const users = await User.find();
@@ -10,13 +10,32 @@ module.exports = {
             res.status(500).json(err);
         }
     },
-    // api/users/ -- creates user
+    // /api/users/ -- creates user
     async createUser(req, res) {
         try {
             const user = await User.create(req.body);
-            req.json(user)
+            res.json(user)
         } catch (err) {
             res.status(500).json(err);
         }
     },
+    // /api/users/:userId -- get single user
+    async getSingleUser(req, res) {
+        try {
+            const user = await User.findOne({ _id: req.params.userId })
+            .select('-__v');
+
+            if (!user) {
+                return res.status(404).json({ message: 'No user with that ID' })
+            }
+
+            res.json(user)
+            
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    },
+    // /api/users/:userId -- update user 
+
+    // /api/users/:userId -- delete user
 }
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,14 +1,14 @@
 const router = require('express').Router();
 const {
   getUsers,
-//   getSingleUser,
-createUser,
+  createUser,
+  getSingleUser,
 } = require('../../controllers/userController');
 
 // /api/users
 router.route('/').get(getUsers).post(createUser);
 
-// // /api/users/:userId
-// router.route('/:userId').get(getSingleUser);
+// /api/users/:userId
+router.route('/:userId').get(getSingleUser);
 
 module.exports = router;
