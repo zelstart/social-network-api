d2bdc934541c300743856572c10bb6031448622d
zelstart <e.pomeroy95@gmail.com>
2023-10-31 22:47:52 -0400

    started the routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,22 @@
+// const router = require('express').Router();
+// const {
+
+// } = require('../../controllers/thoughtController.js');
+
+// // /api/thoughts
+// router.route('/').get(getThoughts).post(createThought);
+
+// // /api/thoughts/:thoughtId
+// router
+//   .route('/:thoughtId')
+//   .get(getSinglethought)
+//   .put(updatethought)
+//   .delete(deletethought);
+
+// // /api/thoughts/:thoughtId/reaction
+// router.route('/:thoughtId/tags').post(addReaction);
+
+// // /api/thoughts/:thoughtId/tags/:reactionId
+// router.route('/:thoughtId/tags/:tagId').delete(removeReaction);
+
+// module.exports = router;
