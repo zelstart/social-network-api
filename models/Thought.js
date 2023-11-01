095c0f7c80e5c8698b49d7f375bc4611823af80a
zelstart <e.pomeroy95@gmail.com>
2023-10-31 21:52:08 -0400

    changed to 12hr time

--- models/Thought.js
+++ models/Thought.js
@@ -18,7 +18,7 @@ const reactionSchema = new Schema({
   createdAt: {
     type: Date,
     default: Date.now,
-    get: (timestamp) => format(timestamp, 'yyyy-MM-dd HH:mm:ss')
+    get: (timestamp) => format(timestamp, 'yyyy-MM-dd hh:mm:ss')
   }
 });
 
@@ -32,7 +32,7 @@ const thoughtSchema = new Schema({
   createdAt: {
     type: Date,
     default: Date.now,
-    get: (timestamp) => format(timestamp, 'yyyy-MM-dd HH:mm:ss')
+    get: (timestamp) => format(timestamp, 'yyyy-MM-dd hh:mm:ss')
   },
   username: {
     type: String,
