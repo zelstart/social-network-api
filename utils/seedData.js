acac74f1235560f1f5daa350054fe211cd193a47
zelstart <e.pomeroy95@gmail.com>
2023-11-01 00:21:45 -0400

    updated seedData

--- utils/seedData.js
+++ utils/seedData.js
@@ -1,8 +1,8 @@
 const connection = require('../config/connection'); 
 const User = require('../models/User'); 
-const Thought = require('../models/Thought')
+const Thought = require('../models/Thought');
 
-// define some initial users and thoughts
+// Define some initial users and thoughts
 const users = [
   {
     username: 'john_doe',
@@ -11,6 +11,14 @@ const users = [
   {
     username: 'jane_doe',
     email: 'jane@example.com'
+  },
+  {
+    username: 'bob_smith',
+    email: 'bob@example.com'
+  },
+  {
+    username: 'susan_smith',
+    email: 'susan@example.com'
   }
 ];
 
@@ -25,29 +33,45 @@ const thoughts = [
   }
 ];
 
-// function to seed the database
+// Function to seed the database
 async function seedDatabase() {
   try {
-    // create users
+    // Delete all existing users
+    await User.deleteMany({});
+
+    // Create users
     const createdUsers = await User.insertMany(users);
 
-    // assign user IDs to thoughts
+    // Assign user IDs to thoughts
     const thoughtsWithUserIds = thoughts.map(thought => {
       const user = createdUsers.find(user => user.username === thought.username);
       return { ...thought, username: user._id };
     });
 
-    // create thoughts
+    // Create thoughts
     await Thought.insertMany(thoughtsWithUserIds);
+
+    // Add friends
+    const john = createdUsers.find(user => user.username === 'john_doe');
+    const jane = createdUsers.find(user => user.username === 'jane_doe');
+    const bob = createdUsers.find(user => user.username === 'bob_smith');
+    const susan = createdUsers.find(user => user.username === 'susan_smith');
+
+    john.friends.push(jane._id);
+    jane.friends.push(john._id, bob._id);
+    bob.friends.push(jane._id, susan._id);
+    susan.friends.push(bob._id);
+
+    await Promise.all([john.save(), jane.save(), bob.save(), susan.save()]);
 
     console.log('Database seeded successfully');
   } catch (error) {
     console.error('Error seeding database:', error);
   } finally {
-    //disconnect from the database after seeding
+    // Disconnect from the database after seeding
     connection.close();
   }
 }
 
-// call the seeding function
+// Call the seeding function
 seedDatabase();
