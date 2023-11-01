4c8d50a459c95a6458c22795aabb53ca15b0f0b5
zelstart <e.pomeroy95@gmail.com>
2023-10-31 23:26:04 -0400

    update and delete routes for users added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const User  = require('../models/User');
+const User = require('../models/User');
 
 module.exports = {
     // /api/users/ -- gets all users
@@ -14,7 +14,7 @@ module.exports = {
     async createUser(req, res) {
         try {
             const user = await User.create(req.body);
-            res.json(user)
+            res.json(user);
         } catch (err) {
             res.status(500).json(err);
         }
@@ -23,19 +23,50 @@ module.exports = {
     async getSingleUser(req, res) {
         try {
             const user = await User.findOne({ _id: req.params.userId })
-            .select('-__v');
+                .select('-__v');
 
             if (!user) {
-                return res.status(404).json({ message: 'No user with that ID' })
+                return res.status(404).json({ message: 'No user with that ID' });
             }
 
-            res.json(user)
-            
+            res.json(user);
+
         } catch (err) {
-            res.status(500).json(err)
+            res.status(500).json(err);
         }
     },
     // /api/users/:userId -- update user 
+    async updateUser(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $set: req.body },
+                { runValidators: true, new: true }
+            )
 
+            if (!user) {
+                return res.status(404).json({ message: 'No user with that ID' });
+            }
+
+            res.json(user);
+
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
     // /api/users/:userId -- delete user
+    async deleteUser(req, res) {
+        try {
+            const user = await User.findOneAndDelete({ _id: req.params.userId });
+
+            if (!user) {
+                return res.status(404).json({ message: 'No user with that ID' });
+              }
+
+            // await Thought.deleteMany({ id: { $in: user.thoughts }});
+            res.json({ message: 'User has been deleted!' })
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
 }
